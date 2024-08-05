28e3efc3c9ce07e3d70c73d1ce822fb0ca38e021
GamallAhmed <gimyahmed028@gmail.com>
2024-08-05 10:42:18 +0300

    third commit

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  navbar: HTMLElement | null = null;
 
+  ngAfterViewInit() {
+    this.navbar = document.querySelector('.navbar');
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (this.navbar) {
+      if (window.scrollY > 100) {
+        this.navbar.classList.add('scrolled');
+      } else {
+        this.navbar.classList.remove('scrolled');
+      }
+    }
+  }
 }
+
